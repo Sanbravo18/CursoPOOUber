675f4d79e879c224e4a97ea59236767e75a53c81
Daniel Bravo <daniel.brv@hotmail.cl>
2021-09-26 15:58:09 -0300

    Se agrega los avances de herencia

--- Java/UberBlack.java
+++ Java/UberBlack.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.Map;
 
 class UberBlack extends Car {
-    Map<String, Map<String,Integer>> typeCarAccepted;
+    Map<String, ArrayList<String,Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
 
     public UberBlack(String license, Account driver, 
-    Map<String, Map<String,Integer>> typeCarAccepted,
+    Map<String, ArrayList<String,Integer>> typeCarAccepted,
     ArrayList<String> seatsMaterial){
         super(license, driver);
         this.typeCarAccepted = typeCarAccepted;
